87a6a4fe9bc8f2b704362ef865e562cdfd65afa7
Ricky Rock <ricky_rickardo87@hotmail.com>
2016-10-04 13:30:37 +0200

    Title

--- RecipeProject/Recipe.cs
+++ RecipeProject/Recipe.cs
@@ -14,9 +14,11 @@ namespace RecipeProject
         public class Recipe
         {
             public string Description { get; set; }
-            public string Namerecipe { get; set; }
+           
+            public string Title { get; set; }
             public Product Product { get; set; }
-        }
+    }
 
+   
     }
 
