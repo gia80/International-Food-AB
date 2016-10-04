370653ad6642e9909e7cb7c62aed3da165d0ff6c
gia80 <gia80@live.se>
2016-10-04 13:44:53 +0200

    Har lagt till en PopulateListBox

--- GUIProject/Form1.cs
+++ GUIProject/Form1.cs
@@ -17,11 +17,12 @@ namespace GUIProject
 
         private List<Recipe> recipes;
         private Serializer serializer;
+
         public string FilePath {get; set;}
         public Form1()
         {
             InitializeComponent();
-
+             
             FilePath =  Environment.CurrentDirectory + @"\Databas.xml";
             serializer = new Serializer(FilePath);
             recipes = serializer.DeserializeRecipes();
@@ -38,6 +39,25 @@ namespace GUIProject
         private void Form1_Load(object sender, EventArgs e)
         {
             
+        }
+
+
+
+        private void PopulateListBox(Product product)
+        {
+            //Tömmer den gamla listan
+            listRecept.Items.Clear();
+
+            foreach (Recipe r in recipes)
+            {
+                //Väljer endast att vissa en kategori
+                if (r.Product == product)
+                {
+                    listRecept.Items.Add(r);
+                }
+
+            } 
+
         }
     }
 }
