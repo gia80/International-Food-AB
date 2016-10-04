30fb387e6f9b9a3778793cbd3aff7ab042a5db31
gia80 <gia80@live.se>
2016-10-04 11:40:44 +0200

    Ändrat till ProductFileSystem

--- InputOuputClass/ProductFileSystem.cs
+++ InputOuputClass/ProductFileSystem.cs
@@ -2,21 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InputOuputClass
 {
     public class ProductFileSystem
     {
         public string FilePath { get; set; }
- 
 
-       public  ProductFileSystem(string filePath)
+        public ProductFileSystem(string filePath)
         {
             FilePath = filePath;
         }
+
         private void WriteProductToFile(List<Recipe> recipe)
         { //hejehaj
             try
@@ -25,7 +22,7 @@ namespace InputOuputClass
                 {
                     foreach (Recipe p in recipe)
                     {
-                        //w.WriteLine("Firstname: " + p.Firstname, Encoding.Unicode);        
+                        //w.WriteLine("Firstname: " + p.Firstname, Encoding.Unicode);
                         w.WriteLine("------------------------------------");
                     }
                 }
@@ -49,11 +46,10 @@ namespace InputOuputClass
                         string text = r.ReadLine();
                         //p.Firstname = text.Substring(text.IndexOf(":") + 1).Trim();
                         text = r.ReadLine();
-                
 
                         r.ReadLine();
-                     
-                       // phonebook.Add(p);
+
+                        // phonebook.Add(p);
                     }
                 }
             }
@@ -62,7 +58,5 @@ namespace InputOuputClass
                 throw;
             }
         }
-
     }
- 
 }
